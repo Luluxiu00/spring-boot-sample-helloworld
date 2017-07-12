687f52843afb2057f1284224d9f9c37968032a78
luluxiu00 <yanbin_usst@163.com>
2017-07-12 16:29:29 +0800

    spring-boot 入门之helloword

---
+++ src/main/java/com/example/springbootsamplehelloworld/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.springbootsamplehelloworld;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	protected static Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
+	@RequestMapping("/")
+	public String helloWord(){
+		logger.debug("访问hello");
+		return "Hello World!";
+	}
+	
+	
+	@RequestMapping("/hello/{name}")
+	public String helloName(@PathVariable("name")String name){
+		logger.debug("访问helloName,Name={}",name);  
+		return "Hello " + name;
+	}
+	
+	
+}
